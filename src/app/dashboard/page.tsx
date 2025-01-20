5c757990fe959ffec528c42234ab501cd590e92b
Juan Tellez <juantellez916@gmail.com>
2025-01-19 21:46:46 -0600

    add Dashbord Page params types

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,13 @@
-export default function DashboardPage() {
+export default async function DashboardPage(props: {
+    searchParams?: Promise<{
+        query?: string;
+        sort?: string;
+        view?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams
+    const query = searchParams?.query || ''
+
     return (
         <p>Links</p>
     )
