bc6ac561c162dced8cc5ffe0c7756ffcaafa66cd
Juan Tellez <juantellez916@gmail.com>
2025-01-19 22:26:00 -0600

    fix Suspense Error

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,12 +2,15 @@ import CreateLink from "@/components/links/create-link";
 import SortLinks from "@/components/links/sort-links";
 import SearchLink from "@/components/links/search-link";
 import ViewMode from "@/components/links/view-mode";
+import { Suspense } from "react";
 
 export default function DashboardLayout(props: { children: React.ReactNode }) {
     return (
         <main className="flex flex-col items-center h-[calc(100vh-4.4rem)] gap-6 pt-6 font-[family-name:var(--font-geist-sans)]">
             <div className="flex container gap-2">
-                <SearchLink />
+                <Suspense fallback={<p>Loading...</p>}>
+                    <SearchLink />
+                </Suspense>
                 <SortLinks />
                 <ViewMode />
                 <CreateLink />
