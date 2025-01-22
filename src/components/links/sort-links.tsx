9f9809977cbaa2fae031d069297439169c7f8627
Juan Tellez <juantellez916@gmail.com>
2025-01-21 19:02:23 -0600

    add SortLinks Component Design

--- src/components/links/sort-links.tsx
+++ src/components/links/sort-links.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import {
     Select,
@@ -7,20 +9,52 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { IoCalendarClearOutline } from "react-icons/io5";
+import { RxCursorArrow } from "react-icons/rx";
 import { TiArrowUnsorted } from "react-icons/ti";
+import { IoLinkOutline } from "react-icons/io5";
 
 export default function SortLinks() {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const currentSort = searchParams.get("sort") || "date";
+
+    const handleSortChange = (term: string) => {
+        const params = new URLSearchParams(searchParams.toString());
+        if (term) {
+            params.set("sort", term);
+        }
+
+        router.replace(`${pathname}?${params.toString()}`);
+    };
+
+    const getIcon = (value: string) => {
+        switch (value) {
+            case "date":
+                return <IoCalendarClearOutline className="text-slate-400" />;
+            case "clicks":
+                return <RxCursorArrow className="text-slate-400" />;
+            case "shortLink":
+                return <IoLinkOutline className="text-slate-400" />;
+            default:
+                return <TiArrowUnsorted className="text-slate-400" />;
+        }
+    };
+
     return (
-        <Select defaultValue="date">
+        <Select defaultValue={currentSort} onValueChange={handleSortChange}>
             <SelectTrigger className="flex max-w-[340px] items-center space-x-2">
-                <TiArrowUnsorted className="text-slate-400" />
-                <SelectValue placeholder="Sort by" />
+                {getIcon(currentSort)}
+                <SelectValue placeholder={`Sort by ${currentSort}`} />
             </SelectTrigger>
             <SelectContent>
                 <SelectGroup>
-                    <SelectItem value="date">Sort by date</SelectItem>
-                    <SelectItem value="clicks">Sort by clicks</SelectItem>
-                    <SelectItem value="shortLink">Sort by short link</SelectItem>
+                    <SelectItem value="date">Sort by Date</SelectItem>
+                    <SelectItem value="clicks">Sort by Clicks</SelectItem>
+                    <SelectItem value="shortLink">Sort by Short Link</SelectItem>
                 </SelectGroup>
             </SelectContent>
         </Select>
