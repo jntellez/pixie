5187b51b2eccad6394aa13f7a4569cd0e580f05e
Juan Tellez <juantellez916@gmail.com>
2025-01-26 03:54:57 -0600

    add responsive Footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,17 +1,26 @@
 import { GoArrowUpRight } from "react-icons/go";
+import ExternalLink from "../ui/external-link";
 
 export default function Footer() {
     return (
         <footer className="bg-white dark:bg-slate-950 border-t-[1px] border-zinc-200 dark:border-slate-800 text-white absolute bottom-0 w-full py-5">
             <div className="flex container justify-between">
-                <a href="https://github.com/jntellez/pixie" target="_blank" rel="noopener noreferrer" className="flex items-center text-sm text-zinc-500 dark:text-slate-500 hover:underline">
-                    <p>Made by Juan Téllez 2025</p>
+                <ExternalLink
+                    href="https://github.com/jntellez/pixie"
+                    className="flex items-center text-sm text-zinc-500 dark:text-slate-500 hover:underline"
+                >
+                    <p className="hidden sm:block">Made by Juan Téllez 2025</p>
+                    <p className="block sm:hidden">Made by Juan</p>
                     <GoArrowUpRight />
-                </a>
-                <a href="https://github.com/pheralb/slug" target="_blank" rel="noopener noreferrer" className="flex items-center text-sm text-zinc-500 dark:text-slate-500 hover:underline">
-                    <p>Inspired by pheralb/slug</p>
+                </ExternalLink>
+                <ExternalLink
+                    href="https://github.com/pheralb/slug"
+                    className="flex items-center text-sm text-zinc-500 dark:text-slate-500 hover:underline"
+                >
+                    <p className="hidden sm:block">Inspired by pheralb/slug</p>
+                    <p className="block sm:hidden">Inspired by slug</p>
                     <GoArrowUpRight />
-                </a>
+                </ExternalLink>
             </div>
         </footer>
     );
