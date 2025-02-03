30378bb65569db6f752cc7fb05d0353aaff90544
Juan Tellez <juantellez916@gmail.com>
2025-02-03 12:38:46 -0600

    update next auth v5

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,4 +1,11 @@
 import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 
-export default { providers: [Google] } satisfies NextAuthConfig;
+export default {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+} satisfies NextAuthConfig;
--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import authConfig from "./auth.config";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
+const prisma = new PrismaClient();
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
