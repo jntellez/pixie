848c6703e2404a998915ab58d0aece35e09b9dcf
Juan Tellez <juantellez916@gmail.com>
2025-07-14 20:50:22 -0600

    fix Redirect Error

--- src/app/[shortUrl]/page.tsx
+++ src/app/[shortUrl]/page.tsx
@@ -1,4 +1,29 @@
+import { notFound, redirect } from "next/navigation"
+import { db } from "@/server/utils/db"
 
-export default async function Page() {
+type Props = {
+    params: {
+        shortUrl: string
+    }
+}
 
+export default async function RedirectPage({ params }: Props) {
+    const shortUrl = params.shortUrl
+
+    const link = await db.link.findUnique({
+        where: { shortUrl },
+    })
+
+    if (!link) {
+        notFound()
+    }
+
+    await db.link.update({
+        where: { id: link.id },
+        data: {
+            clicks: { increment: 1 },
+        },
+    })
+
+    redirect(link.url)
 }
