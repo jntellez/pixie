c1947610c3e1f68311d499c334ce2e4d513cd2c6
Juan Tellez <juantellez916@gmail.com>
2025-07-14 20:33:39 -0600

    fix

--- src/app/[shortUrl]/page.tsx
+++ src/app/[shortUrl]/page.tsx
@@ -1,25 +1,4 @@
-import { notFound, redirect } from "next/navigation";
-import { db } from "@/server/utils/db";
 
-interface PageProps {
-    params: Record<string, string>;
-}
+export default async function Page() {
 
-export default async function Page({ params }: PageProps) {
-    const { shortUrl } = params;
-
-    const link = await db.link.findUnique({
-        where: { shortUrl },
-    });
-
-    if (!link) {
-        notFound();
-    }
-
-    await db.link.update({
-        where: { id: link.id },
-        data: { clicks: { increment: 1 } },
-    });
-
-    redirect(link.url);
 }
