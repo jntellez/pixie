26bb96411021016de6372085e31cf29eccff43f5
Juan Tellez <juantellez916@gmail.com>
2025-07-14 20:55:31 -0600

    fix Redirect Params

--- src/app/[shortUrl]/page.tsx
+++ src/app/[shortUrl]/page.tsx
@@ -1,14 +1,12 @@
 import { notFound, redirect } from "next/navigation"
 import { db } from "@/server/utils/db"
 
-type Props = {
-    params: {
-        shortUrl: string
-    }
+type PageProps = {
+    params: { shortUrl: string }
 }
 
-export default async function RedirectPage({ params }: Props) {
-    const shortUrl = params.shortUrl
+export default async function RedirectPage({ params }: PageProps) {
+    const { shortUrl } = params
 
     const link = await db.link.findUnique({
         where: { shortUrl },
