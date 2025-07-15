9c601067834f74f217986cf9f17cfa8d155c01db
Juan Tellez <juantellez916@gmail.com>
2025-07-14 20:40:09 -0600

    fix auth route

--- src/app/auth/[...nextauth]/route.ts
+++ src/app/auth/[...nextauth]/route.ts
@@ -1,1 +1,6 @@
-export { handlers as GET, handlers as POST } from "@/auth";
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+
+const handler = NextAuth(authConfig);
+
+export { handler as GET, handler as POST };
